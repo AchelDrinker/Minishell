fcf8b3729b47a40609f0f3915223b5f1a1136a54
AchelDrinker <99177027+AchelDrinker@users.noreply.github.com>
2022-10-11 08:17:04 +0200

    Add files via upload

---
+++ minishell/include/minishell.h
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: humartin <humartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 10:09:59 by humartin          #+#    #+#             */
+/*   Updated: 2022/10/10 21:21:00 by humartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __MINISHELL_H__
+#define __MINISHELL_H__
+#define GREEN "\x1b[32m"
+#define RESET "\x1b[0m"
+#define RED "\033[91m"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <signal.h>
+
+
+typedef struct Cell_t
+{
+	char			*data;
+	struct Cell_t	*next;
+}List, Cell;
+
+List	*emptyList(void);
+List	*copyEnv(List *, char **);
+int		isEmptyList(List *);
+long	lenList(List *);
+List	*addAt(List *, char *, int);
+char	*getAt(List *, int);
+void	setAt(List *, char *, int);
+List	*freeAt(List *, int);
+List	*freeList(List *);
+void	printList(List *);
+void	printCell(List *, int, int);
+void	printCell_n(List *, int, int);
+size_t	ft_strlen(const char *);
+char	*ft_strncat(List *, List *, int);
+char	*ft_strnstr(List *, const char *, size_t);
+void	ft_putchar_fd(char, int);
+int		echo_env_n(char *, int, List *);
+int		echo_env(char *, int, List *);
+int		check_echo_env_n(char *, int, List *);
+int		check_echo_env(char *, int, List *);
+void	ft_echo(char *, int, List *);
+void	ft_echo_n(char *, int, List *);
+List	*check_echo(char *, char *, char *, List *);
+int		check_pwd(char *, char *, List *);
+int		check_env (char *, char *, List *);
+List	*check_input(char *, List *);
+List	*check_export(char *, char *, List *);
+List	*ft_unsetenv(List *, char *);
+List	*check_unset(char *, char *, List *);
+int		check_path(List *, char *);
+void	ft_error_path(char *);
+void	ft_putstr_fd(char *, int);
+void	ft_error_echo(char *);
+void	signal_trap(void);
+void	signal_exec(void);
+void	handle_exec(int);
+void	handle_shell(int);
+void	check_exit(char *, char *);
+int		ft_strcmp(char *, char *);
+int		ft_check_quotes(char *, int);
+int		ft_checker_exit(char *);
+void	ft_error_char(char *, int);
+void	check_spe_char(char *);
+int		ft_single_quotes(char *, char *);
+void	error_fun(void);
+void	header(void);
+char	*check_path_cd(List *, char *);
+List	*cd(char *);
+
+#endif
---
+++ minishell/srcs/stack_char.c
@@ -0,0 +1,132 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_char.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: humartin <humartin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/04 10:04:36 by humartin          #+#    #+#             */
+/*   Updated: 2022/10/10 18:18:58 by humartin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+static Cell *createCell(char *);
+
+//Create a cell with the data in argument
+static Cell *createCell(char *data)
+{
+	Cell *cell;
+
+	cell = malloc(sizeof(Cell));
+	if (!cell)
+		return NULL;
+	cell->data = data;
+	cell->next = NULL;
+	return(cell);
+}
+
+// Add a char element to a defined position of a list
+List	*addAt(List *L, char *data, int pos)
+{
+	List *prec;
+	List *cur;
+	int i;
+
+	prec = L;
+	cur = L;
+	i = 0;
+	Cell *cell = createCell(data);
+	if (isEmptyList(L))
+	{
+		return(cell);
+	}
+	if(pos == 0)
+	{
+		cell->next = L;
+		return (cell);
+	}
+	while (i < pos)
+	{
+		i++;
+		prec = cur;
+		cur = cur->next;
+	}
+	prec->next = cell;
+	cell->next = cur;
+	return(L);
+}
+
+//Edit a char element of the list
+void	setAt(List *L, char *data, int pos)
+{
+	int i;
+
+	i = 0;
+	if(pos > lenList(L))
+		return;
+	while(i < pos)
+	{
+		i++;
+		L = L->next;
+	}
+	L->data = data;
+}
+
+//print the char list
+void	printList(List *L)
+{
+	while(L)
+	{
+		printf("%s\n", L->data);
+		L = L->next;
+	}
+}
+
+//print the char cell in argument
+void	printCell(List *L, int pos, int depart)
+{
+	int i;
+	int c;
+	char *str;
+
+	str = malloc(sizeof(Cell));
+	c = 0;
+	i = 0;
+	while(i < pos)
+	{
+		i++;
+		L = L->next;
+	}
+	while(L->data[++depart] != '\0')
+	{
+		str[c] = L->data[depart];
+		c++;
+	}
+	str[c] = '\0';
+	printf("%s\n", str);
+}
+
+void	printCell_n(List *L, int pos, int depart)
+{
+	int i;
+	int c;
+	char *str;
+
+	str = malloc(sizeof(Cell));
+	c = 0;
+	i = 0;
+	while(i < pos)
+	{
+		i++;
+		L = L->next;
+	}
+	while(L->data[++depart] != '\0')
+	{
+		str[c] = L->data[depart];
+		c++;
+	}
+	str[c] = '\0';
+	printf("%s", str);
+}
